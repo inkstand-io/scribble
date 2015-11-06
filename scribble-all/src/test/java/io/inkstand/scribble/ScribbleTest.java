ee3e2457156c1f66de1e72e5cd20f5a6746dd773
Gerald Mücke <gerald@inkstand.io>
2015-11-06 18:36:50 +0100

    SCRIB-55 split scribble into multiple modules

---
+++ scribble-all/src/test/java/io/inkstand/scribble/ScribbleTest.java
@@ -0,0 +1,312 @@
+/*
+ * Copyright 2015 Gerald Muecke, gerald.muecke@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.inkstand.scribble;
+
+import static io.inkstand.scribble.JCRAssert.assertNodeTypeExists;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.jcr.Session;
+import java.net.URL;
+import org.apache.deltaspike.core.api.config.ConfigProperty;
+import org.junit.Test;
+import org.junit.runner.Description;
+import org.junit.runner.RunWith;
+import org.junit.runners.model.Statement;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import io.inkstand.scribble.inject.Injection;
+import io.inkstand.scribble.rules.builder.InMemoryContentRepositoryBuilder;
+import io.inkstand.scribble.rules.builder.JNDIContentRepositoryBuilder;
+import io.inkstand.scribble.rules.builder.MockContentRepositoryBuilder;
+import io.inkstand.scribble.rules.builder.StandaloneContentRepositoryBuilder;
+import io.inkstand.scribble.rules.builder.TemporaryFolderBuilder;
+import io.inkstand.scribble.jcr.rules.InMemoryContentRepository;
+import io.inkstand.scribble.jcr.rules.StandaloneContentRepository;
+import io.inkstand.scribble.rules.ldap.Directory;
+import io.inkstand.scribble.rules.ldap.DirectoryServer;
+
+/**
+ * Created by Gerald on 18.05.2015.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ScribbleTest {
+
+    @Mock
+    private Description description;
+
+    @Test
+    public void testInject() throws Exception {
+
+        //prepare
+        String value = "123";
+
+        //act
+        Injection result = Scribble.inject(value);
+
+        //assert
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testInjectIntoAll() throws Exception {
+        //prepare
+        String var = "123";
+        SimpleInjectionTarget target = new SimpleInjectionTarget();
+
+        //act
+        Scribble.inject(var).intoAll(target);
+
+        //assert
+        assertEquals(var, target.injectionTarget1);
+        assertEquals(var, target.injectionTarget2);
+        assertEquals(var, target.injectionTarget3);
+
+    }
+
+    @Test
+    public void testInjectInto() throws Exception {
+        //prepare
+        String var = "123";
+        SimpleInjectionTarget target = new SimpleInjectionTarget();
+
+        //act
+        Scribble.inject(var).into(target);
+
+        //assert
+        assertEquals(var, target.injectionTarget1);
+        assertNull(var, target.injectionTarget2);
+        assertNull(var, target.injectionTarget3);
+
+    }
+
+    @Test
+    public void testInjectAsConfigPropertyInto() throws Exception {
+        //prepare
+        String var = "123";
+        SimpleInjectionTarget target = new SimpleInjectionTarget();
+
+        //act
+        Scribble.inject(var).asConfigProperty("property1").into(target);
+
+        //assert
+        assertEquals(var, target.injectionTarget2);
+        assertNull(target.injectionTarget1);
+        assertNull(target.injectionTarget3);
+    }
+
+    @Test
+    public void testInjectAsResourceInto() throws Exception {
+        //prepare
+        String var = "123";
+        SimpleInjectionTarget target = new SimpleInjectionTarget();
+
+        //act
+        Scribble.inject(var).asResource().byLookup("java:/any/name").into(target);
+
+        //assert
+        assertEquals(var, target.injectionTarget3);
+        assertNull(target.injectionTarget1);
+        assertNull(target.injectionTarget2);
+    }
+
+    @Test
+    public void testInjectPrimitiveAsConfigPropertyInto() throws Exception {
+        //prepare
+        int var = 123;
+        SimpleInjectionTarget target = new SimpleInjectionTarget();
+
+        //act
+        Scribble.inject(var).asConfigProperty("property2").into(target);
+
+        //assert
+        assertEquals(var, target.primitiveInt);
+    }
+
+    @Test
+    public void testNewTempFolder() throws Exception {
+
+        //act
+        TemporaryFolderBuilder result = Scribble.newTempFolder();
+
+        //assert
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testNewMockContentRepository() throws Exception {
+
+        //act
+        MockContentRepositoryBuilder result = Scribble.newMockContentRepository();
+
+        //assert
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testNewJNDIContextRepository() throws Exception {
+
+        //act
+        JNDIContentRepositoryBuilder result = Scribble.newJndiContentRepository();
+
+        //assert
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testNewInMemoryContentRepository() throws Exception {
+
+        //act
+        InMemoryContentRepositoryBuilder result = Scribble.newInMemoryContentRepository();
+
+        //assert
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testNewInMemoryContentRepository_withCnd() throws Throwable {
+
+        //prepare
+        final URL cndModel = getClass().getResource("ScribbleTest_testModel.cnd");
+
+        //act
+        //this is the actual line how the rule builder should be used in a test
+        final InMemoryContentRepository result = Scribble.newInMemoryContentRepository()
+                                                         .withNodeTypes(cndModel)
+                                                         .build();
+
+        //assert
+        assertNotNull(result);
+        result.apply(new Statement() {
+
+            @Override
+            public void evaluate() throws Throwable {
+
+                final Session session = result.login("admin", "admin");
+                assertNodeTypeExists(session, "test:testType");
+            }
+        }, description).evaluate();
+    }
+
+    @Test
+    public void testNewStandaloneContentRepository() throws Exception {
+        //act
+        StandaloneContentRepositoryBuilder result = Scribble.newStandaloneContentRepository();
+
+        //assert
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testNewStandaloneContentRepository_withCnd() throws Throwable {
+
+        //prepare
+        final URL cndModel = getClass().getResource("ScribbleTest_testModel.cnd");
+
+        //act
+        //this is the actual line how the rule builder should be used in a test
+        final StandaloneContentRepository result = Scribble.newStandaloneContentRepository()
+                                                           .withNodeTypes(cndModel)
+                                                           .build();
+
+        //assert
+        assertNotNull(result);
+
+        result.apply(new Statement() {
+
+            @Override
+            public void evaluate() throws Throwable {
+
+                final Session session = result.login("admin", "admin");
+                assertNodeTypeExists(session, "test:testType");
+            }
+        }, description).evaluate();
+    }
+
+    @Test
+    public void testNewDirectory() throws Exception {
+        //prepare
+
+        //act
+        final Directory dir = Scribble.newDirectory().build();
+
+        //assert
+        assertNotNull(dir);
+
+    }
+
+    @Test
+    public void testNewDirectoryServer() throws Exception {
+        //prepare
+
+        //act
+        final DirectoryServer ds = Scribble.newDirectoryServer().build();
+
+        //assert
+        assertNotNull(ds);
+    }
+
+    /**
+     * Test to verify the rule is properly applicable and initialized with users from an ldif.
+     *
+     * @throws Throwable
+     */
+    @Test
+    public void testNewDirectoryServerWithContent() throws Throwable {
+        //prepare
+
+        //act
+        final URL ldif = getClass().getResource("ScribbleTest_testUsers.ldif");
+        final DirectoryServer ds = Scribble.newDirectory()
+                                           .withPartition("inkstand", "dc=scribble")
+                                           .importLdif(ldif)
+                                           .aroundDirectoryServer()
+                                           .onAvailablePort()
+                                           .build();
+        //assert
+        assertNotNull(ds);
+        ds.apply(new Statement() {
+
+            @Override
+            public void evaluate() throws Throwable {
+
+                assertTrue(ds.getDirectoryService().getSession().exists("uid=testuser,ou=users,dc=scribble"));
+            }
+        }, description).evaluate();
+    }
+
+    static class SimpleInjectionTarget {
+
+        String injectionTarget1;
+
+        @Inject
+        @ConfigProperty(name = "property1")
+        String injectionTarget2;
+
+        @Resource(lookup = "java:/any/name")
+        String injectionTarget3;
+
+        @Inject
+        @ConfigProperty(name = "property2")
+        int primitiveInt;
+    }
+}
---
+++ scribble-jcr/src/test/java/io/inkstand/scribble/jcr/rules/util/XMLContentHandlerTest.java
@@ -0,0 +1,342 @@
+/*
+ * Copyright 2015 Gerald Muecke, gerald.muecke@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.inkstand.scribble.jcr.rules.util;
+
+import static io.inkstand.scribble.JCRAssert.assertMixinNodeType;
+import static io.inkstand.scribble.JCRAssert.assertNodeExistByPath;
+import static io.inkstand.scribble.JCRAssert.assertNodeNotExistByPath;
+import static io.inkstand.scribble.JCRAssert.assertPrimaryNodeType;
+import static io.inkstand.scribble.JCRAssert.assertStringPropertyEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+
+import javax.jcr.Node;
+import javax.jcr.Session;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.xml.sax.Attributes;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.AttributesImpl;
+
+import io.inkstand.scribble.Scribble;
+import io.inkstand.scribble.jcr.rules.ContentRepository;
+
+public class XMLContentHandlerTest {
+
+    private static final String INK_NODE = "ink:node";
+    private static final String NODE = "node";
+    private static final String PROPERTY = "property";
+    private static final String INK_PROPERTY = "ink:property";
+    private static final String MIXIN = "mixin";
+    private static final String INK_MIXIN = "ink:mixin";
+    private static final String INK_ROOT_NODE = "ink:rootNode";
+    private static final String ROOT_NODE = "rootNode";
+    private static final String INKSTAND_IMPORT_NAMESPACE = "http://inkstand.io/schemas/rules-import";
+    @Rule
+    public final ContentRepository repository = Scribble.newTempFolder().aroundInMemoryContentRepository().build();
+    private Session adminSession = null;
+    private XMLContentHandler subject;
+
+    @Before
+    public void setUp() throws Exception {
+
+        adminSession = repository.getAdminSession();
+        subject = new XMLContentHandler(adminSession);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+        if (adminSession != null) {
+            adminSession.logout();
+        }
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testError() throws Exception {
+        //prepare
+        Locator locator = mock(Locator.class);
+        SAXParseException sx = new SAXParseException("test", locator);
+
+        //act
+        subject.error(sx);
+    }
+
+    @Test
+    public void testEventFlow_01_simpleStructure() throws Exception {
+        // act
+        ns_eventFlow_rootNode(INKSTAND_IMPORT_NAMESPACE);
+        // assert
+        final Session session = repository.getAdminSession();
+        assertNodeExistByPath(session, "/root");
+        final Node rootNode = session.getNode("/root");
+        assertPrimaryNodeType(rootNode, "nt:unstructured");
+        assertMixinNodeType(rootNode, "mix:title");
+        assertStringPropertyEquals(rootNode, "rules:title", "TestTitle");
+
+        Node node = subject.getRootNode();
+        node.refresh(true);
+        assertNotNull(node);
+        assertEquals(node.getPath(), rootNode.getPath());
+    }
+
+    /**
+     * A simple flow of events that creates an unstructured root node
+     *
+     * @param namespace
+     *         the namespace for all element events
+     *
+     * @throws SAXException
+     */
+    protected void ns_eventFlow_rootNode(final String namespace) throws SAXException {
+
+        subject.startDocument();
+        subject.startPrefixMapping("ink", namespace);
+
+        subject.startElement(namespace,
+                             ROOT_NODE,
+                             INK_ROOT_NODE,
+                             createAttributes("name", "root", "primaryType", "nt:unstructured"));
+
+        subject.startElement(namespace, MIXIN, INK_MIXIN, createAttributes("name", "mix:title"));
+        subject.endElement(namespace, MIXIN, INK_MIXIN);
+
+        subject.startElement(namespace,
+                             PROPERTY,
+                             INK_PROPERTY,
+                             createAttributes("name", "rules:title", "jcrType", "STRING"));
+        subject.characters("TestTitle".toCharArray(), 0, 9);
+        subject.endElement(namespace, PROPERTY, INK_PROPERTY);
+
+        subject.endElement(namespace, ROOT_NODE, INK_ROOT_NODE);
+        subject.endPrefixMapping("ink");
+        subject.endDocument();
+    }
+
+    private Attributes createAttributes(final String... att) {
+
+        assertEquals("list must be name-value pairs", 0, att.length % 2);
+        final AttributesImpl attr = new AttributesImpl();
+        for (int i = 0, len = att.length; i < len; i += 2) {
+            // no namespace & qname for attributes
+            attr.addAttribute("", att[i], att[i], "CDATA", att[i + 1]);
+        }
+        return attr;
+    }
+
+    @Test
+    public void testEventFlow_01_ignoredNamespace() throws Exception {
+        // act
+        ns_eventFlow_rootNode("ignore");
+        // assert
+        final Session session = repository.getAdminSession();
+        // nothing is created at all
+        assertNodeNotExistByPath(session, "/root");
+    }
+
+    @Test
+    public void testEventFlow_02_ignoredElements() throws Exception {
+        // act
+        ns_eventFlow_ignoredElements();
+        // assert
+        final Session session = repository.getAdminSession();
+        // nothing is created at all
+        assertNodeNotExistByPath(session, "/root");
+    }
+
+    /**
+     * An event flow with ignored elements. The elements in this flow are not defined in the schema.
+     *
+     * @throws SAXException
+     */
+    protected void ns_eventFlow_ignoredElements() throws SAXException {
+
+        final String namespace = INKSTAND_IMPORT_NAMESPACE;
+        subject.startDocument();
+        subject.startPrefixMapping("ink", namespace);
+
+        subject.startElement(namespace,
+                             "ignore",
+                             "ink:ignore",
+                             createAttributes("name", "root", "primaryType", "nt:unstructured"));
+
+        subject.startElement(namespace, "ignoreMixin", "ink:ignoreMixin", createAttributes("name", "mix:title"));
+        subject.endElement(namespace, "ignoreMixin", "ink:ignoreMixin");
+
+        subject.startElement(namespace,
+                             "ignoreProperty",
+                             "ink:ignoreProperty",
+                             createAttributes("name", "rules:title", "jcrType", "STRING"));
+        subject.characters("TestTitle".toCharArray(), 0, 9);
+        subject.endElement(namespace, "ignoreProperty", "ink:ignoreProperty");
+
+        subject.endElement(namespace, "ignore", "ink:ignore");
+        subject.endPrefixMapping("ink");
+        subject.endDocument();
+    }
+
+    @Test
+    public void testEventFlow_03_simpleNestedStructure() throws Exception {
+        // act
+        ns_eventFlow_rootAndChildNode(INKSTAND_IMPORT_NAMESPACE);
+        // assert
+        final Session session = repository.getAdminSession();
+        assertNodeExistByPath(session, "/root");
+        assertNodeExistByPath(session, "/root/child");
+        final Node rootNode = session.getNode("/root");
+        assertPrimaryNodeType(rootNode, "nt:unstructured");
+        assertMixinNodeType(rootNode, "mix:title");
+        assertStringPropertyEquals(rootNode, "rules:title", "TestTitle");
+
+        final Node childNode = session.getNode("/root/child");
+        assertPrimaryNodeType(childNode, "nt:unstructured");
+        assertMixinNodeType(childNode, "mix:title");
+        assertStringPropertyEquals(childNode, "rules:title", "ChildNode");
+    }
+
+    /**
+     * A simple flow of events that creates an unstructured root node with one child node
+     *
+     * @param namespace
+     *         the namespace for all element events
+     *
+     * @throws SAXException
+     */
+    protected void ns_eventFlow_rootAndChildNode(final String namespace) throws SAXException {
+
+        subject.startDocument();
+        subject.startPrefixMapping("ink", namespace);
+
+        subject.startElement(namespace,
+                             ROOT_NODE,
+                             INK_ROOT_NODE,
+                             createAttributes("name", "root", "primaryType", "nt:unstructured"));
+
+        subject.startElement(namespace, MIXIN, INK_MIXIN, createAttributes("name", "mix:title"));
+        subject.endElement(namespace, MIXIN, INK_MIXIN);
+
+        subject.startElement(namespace,
+                             PROPERTY,
+                             INK_PROPERTY,
+                             createAttributes("name", "rules:title", "jcrType", "STRING"));
+        subject.characters("TestTitle".toCharArray(), 0, 9);
+        subject.endElement(namespace, PROPERTY, INK_PROPERTY);
+
+        // start child node
+        subject.startElement(namespace,
+                             NODE,
+                             INK_NODE,
+                             createAttributes("name", "child", "primaryType", "nt:unstructured"));
+        subject.startElement(namespace, MIXIN, INK_MIXIN, createAttributes("name", "mix:title"));
+        subject.endElement(namespace, MIXIN, INK_MIXIN);
+        subject.startElement(namespace,
+                             PROPERTY,
+                             INK_PROPERTY,
+                             createAttributes("name", "rules:title", "jcrType", "STRING"));
+        subject.characters("ChildNode".toCharArray(), 0, 9);
+        subject.endElement(namespace, PROPERTY, INK_PROPERTY);
+        subject.endElement(namespace, NODE, INK_NODE);
+        // end child node
+
+        subject.endElement(namespace, ROOT_NODE, INK_ROOT_NODE);
+        subject.endPrefixMapping("ink");
+        subject.endDocument();
+    }
+
+    @Test
+    public void testEventFlow_04_structureWithEmptyText() throws Exception {
+        // act
+        ns_eventFlow_rootNode_emptyTitle(INKSTAND_IMPORT_NAMESPACE);
+        // assert
+        final Session session = repository.getAdminSession();
+        assertNodeExistByPath(session, "/root");
+        final Node rootNode = session.getNode("/root");
+        assertPrimaryNodeType(rootNode, "nt:unstructured");
+        assertMixinNodeType(rootNode, "mix:title");
+        assertStringPropertyEquals(rootNode, "rules:title", "");
+    }
+
+    /**
+     * A simple flow of events that creates an unstructured root node with an empty title
+     *
+     * @param namespace
+     *         the namespace for all element events
+     *
+     * @throws SAXException
+     */
+    protected void ns_eventFlow_rootNode_emptyTitle(final String namespace) throws SAXException {
+
+        subject.startDocument();
+        subject.startPrefixMapping("ink", namespace);
+
+        subject.startElement(namespace,
+                             ROOT_NODE,
+                             INK_ROOT_NODE,
+                             createAttributes("name", "root", "primaryType", "nt:unstructured"));
+
+        subject.startElement(namespace, MIXIN, INK_MIXIN, createAttributes("name", "mix:title"));
+        subject.endElement(namespace, MIXIN, INK_MIXIN);
+
+        subject.startElement(namespace,
+                             PROPERTY,
+                             INK_PROPERTY,
+                             createAttributes("name", "rules:title", "jcrType", "STRING"));
+        //title is empty
+        subject.characters("".toCharArray(), 0, 0);
+        subject.endElement(namespace, PROPERTY, INK_PROPERTY);
+
+        subject.endElement(namespace, ROOT_NODE, INK_ROOT_NODE);
+        subject.endPrefixMapping("ink");
+        subject.endDocument();
+    }
+
+    /**
+     * A simple flow of events that creates an unstructured root node
+     *
+     * @throws Exception
+     */
+    protected void eventFlow_ignoredNameSpace() throws Exception {
+
+        subject.startDocument();
+        subject.startPrefixMapping("ink", "");
+
+        subject.startElement(INKSTAND_IMPORT_NAMESPACE,
+                             ROOT_NODE,
+                             INK_ROOT_NODE,
+                             createAttributes("name", "root", "primaryType", "nt:unstructured"));
+
+        subject.startElement(INKSTAND_IMPORT_NAMESPACE, MIXIN, INK_MIXIN, createAttributes("name", "mix:title"));
+        subject.endElement(INKSTAND_IMPORT_NAMESPACE, MIXIN, INK_MIXIN);
+
+        subject.startElement(INKSTAND_IMPORT_NAMESPACE,
+                             PROPERTY,
+                             INK_PROPERTY,
+                             createAttributes("name", "rules:title", "jcrType", "STRING"));
+        subject.characters("TestTitle".toCharArray(), 0, 9);
+        subject.endElement(INKSTAND_IMPORT_NAMESPACE, PROPERTY, INK_PROPERTY);
+
+        subject.endElement(INKSTAND_IMPORT_NAMESPACE, ROOT_NODE, INK_ROOT_NODE);
+        subject.endPrefixMapping("ink");
+        subject.endDocument();
+    }
+
+}
