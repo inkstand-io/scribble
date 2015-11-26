dc90ad7c477348ec5bbda061ba108c36772394aa
Gerald Mücke <gerald@inkstand.io>
2015-11-26 08:20:01 +0100

    SCRIB-49 added context class loader support

--- scribble-api/src/main/java/io/inkstand/scribble/util/CallStack.java
+++ scribble-api/src/main/java/io/inkstand/scribble/util/CallStack.java
@@ -19,19 +19,40 @@ public final class CallStack {
      * For example: <br>
      * A.calling() -> B.called()
      * B.called() -> getCallerClass(): A
-     *
+     * <br>
+     * If a thread context classloader is defined, it will be used for loading the class, otherwise the
+     * default class loader is used.
      * @return
      *  the class of the calling method's class
      */
     public static Class<?> getCallerClass(){
         final StackTraceElement[] stElements = Thread.currentThread().getStackTrace();
         try {
-            return Class.forName(stElements[2].getClassName());
+            StackTraceElement caller = stElements[2];
+            return loadClass(caller.getClassName());
         } catch (ClassNotFoundException e) {
             LOG.debug("Could not determine caller class", e);
             return null;
 
         }
+    }
+
+    /**
+     * Loads the class specified by name using the Thread's context class loader - if defined - otherwise the
+     * default classloader.
+     * @param className
+     *  the name of the class to load
+     * @return
+     *  the loaded class
+     * @throws ClassNotFoundException
+     *  if the the class could not be loaded
+     */
+    private static Class<?> loadClass(final String className) throws ClassNotFoundException {
+        ClassLoader ctxCL = Thread.currentThread().getContextClassLoader();
+        if(ctxCL == null) {
+            return Class.forName(className);
+        }
+        return ctxCL.loadClass(className);
     }
 
 }
--- scribble-api/src/test/java/io/inkstand/scribble/util/CallStackTest.java
+++ scribble-api/src/test/java/io/inkstand/scribble/util/CallStackTest.java
@@ -1,16 +1,37 @@
 package io.inkstand.scribble.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 /**
  * Created by Gerald Muecke on 26.11.2015.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class CallStackTest {
 
+    @Mock
+    private ClassLoader classLoader;
+    private ClassLoader origCtxCl;
+
+    @Before
+    public void setUp() throws Exception {
+        this.origCtxCl = Thread.currentThread().getContextClassLoader();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Thread.currentThread().setContextClassLoader(origCtxCl);
+    }
+
     @Test
-    public void testGetCallerClass() throws Exception {
+    public void testGetCallerClass_defaultClassLoader() throws Exception {
 
         //prepare
 
@@ -18,6 +39,23 @@ public class CallStackTest {
         Class<?> cls = CallStack.getCallerClass();
 
         //assert
-        assertEquals(this.getClass(), cls );
+        assertEquals(this.getClass(), cls);
+    }
+    @Test
+    public void testGetCallerClass_contextClassLoader() throws Exception {
+
+        //prepare
+        when(classLoader.loadClass(CallStackTest.class.getName())).thenReturn((Class)MockClass.class);
+        Thread.currentThread().setContextClassLoader(classLoader);
+
+        //act
+        Class<?> cls = CallStack.getCallerClass();
+
+        //assert
+        assertEquals(MockClass.class, cls);
+    }
+
+    public static class MockClass extends CallStackTest {
+
     }
 }
